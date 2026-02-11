26ff699688c993bba9e07daa18fea3f2dadfd51d
Amitk kumar prasad <amitprasad8112@gmail.com>
2026-02-11 15:38:52 +0530

    linklist basic

--- linkList/basic.java
+++ linkList/basic.java
@@ -1,13 +1,125 @@
-class basic{
-    public static Class Node{
+public class basic {
+    public static class Node{
         int data;
         Node next;
-        public node(int data){
+
+    public Node(int data){
             this.data=data;
             this.next=null; 
         }
+       
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data){
+         //step 1 create new node
+        Node newNode =new Node(data);
+        size++;
+        if(head==null){
+          head=tail=newNode;
+          return;
+        }
+        //step 2 newNode next=head
+        newNode.next=head;
+
+        //step 3 head=newNode
+        head=newNode;
+    }
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        size++;
+         if(head==null){
+            head=tail=newNode;
+            return;
+         }
+        tail.next=newNode;
+
+        tail=newNode;
+    }
+    public void print(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+      }
+        System.out.println("null");
+    }
+    public void add (int idx,int data){
+        if(idx==0){
+            addFirst(data);
+            return;
+        }
+        Node newNode=new Node(data);
+        size++;
+        Node temp=head;
+        int i=0;
+        while(i<idx-1){
+            temp=temp.next;
+            i++;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+    public int removeFirst(){
+        if(size==0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }else if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size=0 ;
+            return val;
+        }
+        int val=head.data;
+        head=head.next;
+        size--;
+        return val;
+    }
+    public int  removeLast(){
+        if(size==0){
+            System.out.println("ll is empty");
+            return Integer.MIN_VALUE;
+        }else if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        Node prev=head;
+        for(int i=0;i<size-2;i++){
+            prev=prev.next;
+        }
+        int val=prev.next.data;
+        prev.next=null;
+        tail=prev;
+        size--;
+        return val;
+    }
+    public int Serch(int key){
+        Node temp=head;
+        int i=0;
+        while(temp!=null){
+            if(temp.data==key){
+                return i;
+            }
+            temp=temp.next;
+            i++;
+        }return -1;
     }
     public static void main(String[] args) {
-
+        basic ll=new basic();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.add(2, 3);
+        ll.print();
+        // System.out.println("Size: " + size);
+        // ll.removeFirst();
+        // ll.removeLast();
+        // ll.print();
+        System.out.println(ll.Serch(4));
     }
 }
