acab639ae98bf23c746045e12d2a4d314cbdd0e1
Amitk kumar prasad <amitprasad8112@gmail.com>
2026-02-12 20:28:46 +0530

    reverse the linklist and remove the link list

--- linkList/basic.java
+++ linkList/basic.java
@@ -1,115 +1,177 @@
 public class basic {
-    public static class Node{
+    public static class Node {
         int data;
         Node next;
 
-    public Node(int data){
-            this.data=data;
-            this.next=null; 
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
-       
+
     }
+
     public static Node head;
     public static Node tail;
     public static int size;
 
-    public void addFirst(int data){
-         //step 1 create new node
-        Node newNode =new Node(data);
+    public void addFirst(int data) {
+        // step 1 create new node
+        Node newNode = new Node(data);
         size++;
-        if(head==null){
-          head=tail=newNode;
-          return;
-        }
-        //step 2 newNode next=head
-        newNode.next=head;
-
-        //step 3 head=newNode
-        head=newNode;
-    }
-    public void addLast(int data){
-        Node newNode=new Node(data);
-        size++;
-         if(head==null){
-            head=tail=newNode;
+        if (head == null) {
+            head = tail = newNode;
             return;
-         }
-        tail.next=newNode;
+        }
+        // step 2 newNode next=head
+        newNode.next = head;
 
-        tail=newNode;
+        // step 3 head=newNode
+        head = newNode;
     }
-    public void print(){
-        Node temp=head;
-        while(temp!=null){
-            System.out.print(temp.data+"->");
-            temp=temp.next;
-      }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+
+        tail = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
         System.out.println("null");
     }
-    public void add (int idx,int data){
-        if(idx==0){
+
+    public void add(int idx, int data) {
+        if (idx == 0) {
             addFirst(data);
             return;
         }
-        Node newNode=new Node(data);
+        Node newNode = new Node(data);
         size++;
-        Node temp=head;
-        int i=0;
-        while(i<idx-1){
-            temp=temp.next;
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
             i++;
         }
-        newNode.next=temp.next;
-        temp.next=newNode;
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
-    public int removeFirst(){
-        if(size==0){
+
+    public int removeFirst() {
+        if (size == 0) {
             System.out.println("LL is empty");
             return Integer.MIN_VALUE;
-        }else if(size==1){
-            int val=head.data;
-            head=tail=null;
-            size=0 ;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
             return val;
         }
-        int val=head.data;
-        head=head.next;
+        int val = head.data;
+        head = head.next;
         size--;
         return val;
     }
-    public int  removeLast(){
-        if(size==0){
+
+    public int removeLast() {
+        if (size == 0) {
             System.out.println("ll is empty");
             return Integer.MIN_VALUE;
-        }else if(size==1){
-            int val=head.data;
-            head=tail=null;
-            size=0;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
             return val;
         }
-        Node prev=head;
-        for(int i=0;i<size-2;i++){
-            prev=prev.next;
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
         }
-        int val=prev.next.data;
-        prev.next=null;
-        tail=prev;
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
         size--;
         return val;
     }
-    public int Serch(int key){
-        Node temp=head;
-        int i=0;
-        while(temp!=null){
-            if(temp.data==key){
+
+    public int helper(Node head, int key) {// 0(n)
+        if (head == null) {
+            return -1;
+        }
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    public int Serch(int key) {
+        Node temp = head;
+        int i = 0;
+        while (temp != null) {
+            if (temp.data == key) {
                 return i;
             }
-            temp=temp.next;
+            temp = temp.next;
             i++;
-        }return -1;
+        }
+        return -1;
     }
+
+    public void reverse() {
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+    public void deleteNth(int n){
+        int sz=0;
+       Node temp=head;
+       while(temp!=null){
+        temp=temp.next;
+        sz++;
+       }
+       if(n==sz){
+        head=head.next;
+        return;
+       }
+       int i=1;
+       Node pre=head;
+       int iTofind=sz-n;
+       while(i<iTofind){
+        pre=pre.next;
+        i++;
+       }pre.next=pre.next.next;
+       return;
+
+       
+    }
+
     public static void main(String[] args) {
-        basic ll=new basic();
+        basic ll = new basic();
         ll.addFirst(2);
         ll.addFirst(1);
         ll.addLast(4);
@@ -120,6 +182,10 @@ public class basic {
         // ll.removeFirst();
         // ll.removeLast();
         // ll.print();
-        System.out.println(ll.Serch(4));
+        // System.out.println(ll.Serch(3));
+        // ll.reverse();
+        // ll.print();
+         ll.deleteNth(3); ll.print();
+
     }
 }
